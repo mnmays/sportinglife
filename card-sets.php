43133d253d2e3e80e9f13480e2fac89a304ef3e3
mnmays <mnmays@umich.edu>
2017-05-18 23:06:45 -0400

    continuing to re-build project

---
+++ card-sets.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	
+</head>
+
+<header>
+	<div class="header">
+		<img src="images/logo.png" id="logo" alt="sporting life logo">
+	</div>		
+</header>
+
+<nav>
+	
+</nav>
+
+<body>
+	<aside>
+		
+	</aside>
+
+	<article>
+		
+	</article>
+</body>
+
+<footer>
+	
+</footer>
+</html>
