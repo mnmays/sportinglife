bf4064da62227eb174957b435e315bc26ec178f9
mnmays <mnmays@umich.edu>
2017-05-19 13:51:12 -0400

    progress towards combining all card sets info in to one page

--- card-sets.php
+++ card-sets.php
@@ -16,12 +16,40 @@
 
 <body>
 	<aside>
-		
-	</aside>
+		<?php
+
+ $dbhost = "localhost";
+ $dbuser = "root";
+ $dbpass = "M3g4nM4ys17";
+ $db = "sportinglife";
+ 
+ 
+ $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+ 
+/*query*/
+ $query = "SELECT * FROM series";
+ $result = mysqli_query($conn, $query);
+ 
+ $numRows = mysqli_num_rows($result);
+  
+ for($i=0; $i < $numRows; $i++)
+ {
+ 	  $row = mysqli_fetch_array($result);
+	  ?>
+	  <button onclick"showPics()"><?php echo $row['seriesName'];?></button> <br>
+	  <?php
+	  
+ } ?>
+ 
+ 
+ 
+ 	</aside>
 
 	<article>
-		
+
+
 	</article>
+
 </body>
 
 <footer>
