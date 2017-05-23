3a474e443ebff38f5668dbc6d5be20387685db0e
mnmays <mnmays@umich.edu>
2017-05-23 03:38:53 -0400

    working on connecting w/  sporting life page & general styles sheet

---
+++ connect.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<link rel="stylesheet" type="text/css" href="styles/generalStyles.css">
+	<link rel="stylesheet" type="text/css" href="styles/connection.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script type="text/javascript" src="JS/connpgJscript.js"></script>
+</head>		
+<body>
+<header>
+	<header style="
+	background-image:url(images/comerica-park-artwork.jpg); 
+	background-size: cover;
+	" >
+	<img src="images/logo.png" id="logo" alt="sporting life logo">
+</header>
+<nav>
+		<ul>
+			<li class="active">
+				<a href="products.php" id="products">Products</a>	
+			</li>
+			<li class="active">
+				<a href="card-sets.php" id="cardSets">Card Sets</a>
+			</li>
+			<li class="active">
+				<a href="about-sporting-life.html" id="abtCreator">About the Creator</a>
+			</li>
+			<li class="active">
+				<a href="connect.php" id="connect">Connect with Sporting Life</a>
+			</li>
+		</ul>
+</nav>
+
+<aside>
+<p>
+<!----Twitter drop down---->
+	<div id="Twitflip">Twitter</div>
+	<div id="twitTmln">
+			<a class="twitter-timeline" href="https://twitter.com/SportingLifeArt">Tweets by SportingLifeArt</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+	</div>
+</p>
+<p>	
+<!----Facebook drop down---->	
+	<div id="FBflip">Facebook</div>
+	<div id="FBTmln">
+		<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FSportingLifeCards%2F&tabs=timeline&width=300&height=500&small_header=true&adapt_container_width=true&hide_cover=true&show_facepile=false&appId" width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+	</div>
+</p>	
+</aside>
+
+<article>
+	<div id="pageBody">
+
+			this is the article
+
+		
+		<iframe src="http://meganmays.blogspot.com" width="40%" height="500">
+			</iframe>
+	</div>
+
+</article>
+
+<footer>
+			Connect with Sporting Life: 
+		<a href="https://twitter.com/SportingLifeArt?ref_src=twsrc%5Etfw&ref_url=http%3A%2F%2F127.0.0.1%3A8020%2Fsportsentities.home%2Fconnect.html">
+			<img src="images/twitterlogo.png" alt="twitter icon" id="TwitLogo"/></a>
+			<a href="https://www.facebook.com/SportingLifeCards/"><img src="images/facebooklogo.png" alt="facebook icon" id="FBLogo"/></a>
+					<a href="https://www.pinterest.com/jandrews3d/sporting-life-art-cards-collectibles/?fb_ref=528962056142023372%3Acba652a7869654e0e616">
+ 			<img src="images/pintlogo.png" alt ="pinterest icon" id="pinLogo"/>
+ 		</a>
+</footer>
+</body>	
+</html>
