ace74a0debc68da4316d16ec126efaffd6c73a7f
mnmays <mnmays@umich.edu>
2017-06-04 04:14:46 -0400

    added styling for About Sporting Life page & Connection page.
    Finished email subscription form on Connection Page

---
+++ form.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="styles/connection.css">
+		<script type="text/javascript" src="JS/connpgJscript.js"></script>
+	</head>
+		
+		<form id="form" name="form" type="post" onsubmit="return validate()" action="submitEmail.php">
+		<h2>Subscribe to Sporting Life Emails</h2>
+		<hr>
+		<input id="email" name="email" placeholder="Email" type="email">		<!-- Email text box-->
+		<p>
+		<input id="submit" name="submit" value="Subscribe" type="submit">		<!-- Submit Button-->
+		</p>
+	</form>
+</html>
