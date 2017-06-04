44ec77f3a5ca46173de2d8e145c46f6f51aed498
mnmays <mnmays@umich.edu>
2017-06-04 14:25:04 -0400

    styles

--- card-sets.php
+++ card-sets.php
@@ -35,15 +35,20 @@ function showPics(str) {
 </header>
 
 <nav>
-	<nav style="
-	background-image:url(images/comerica-park-artwork.jpg); 
-	background-size: cover;
-	background-attachment: fixed;
-	" >
-	<a href="products.php" class="button">Products</a>
-	<a href="card-sets.php" class="button">Card Sets</a>
-	<a href="about-sporting-life.html" class="button">About Sporting Life</a>
-	<a href="connect.php" class="button">Connect with Sporting Life</a>
+		<ul>
+			<li class="active">
+				<a href="products.php" id="products">Products</a>	
+			</li>
+			<li class="active">
+				<a href="card-sets.php" id="cardSets">Card Sets</a>
+			</li>
+			<li class="active">
+				<a href="about-sporting-life.html" id="abtCreator">About the Creator</a>
+			</li>
+			<li class="active">
+				<a href="connect.php" id="connect">Connect with Sporting Life</a>
+			</li>
+		</ul>
 </nav>
 
 <body>
@@ -108,20 +113,15 @@ function showPics(str) {
 
 	</article>
 
-</body>
 
 <footer>
-			<div id="SMlinks">
 			Connect with Sporting Life: 
 		<a href="https://twitter.com/SportingLifeArt?ref_src=twsrc%5Etfw&ref_url=http%3A%2F%2F127.0.0.1%3A8020%2Fsportsentities.home%2Fconnect.html">
-			<img src="images/twitterlogo.png" alt="twitter icon" id="TwitLogo"/>
+			<img src="images/twitterlogo.png" alt="twitter icon" id="TwitLogo"/></a>
+			<a href="https://www.facebook.com/SportingLifeCards/"><img src="images/facebooklogo.png" alt="facebook icon" id="FBLogo"/></a>
+					<a href="https://www.pinterest.com/jandrews3d/sporting-life-art-cards-collectibles/?fb_ref=528962056142023372%3Acba652a7869654e0e616">
+ 			<img src="images/pintlogo.png" alt ="pinterest icon" id="pinLogo"/>
  		</a>
- 		<a href="https://www.facebook.com/SportingLifeCards/">
- 			<img src="images/facebooklogo.png" alt="facebook icon" id="FBLogo"/> 
-		</a>
- 		<a href="https://www.pinterest.com/jandrews3d/sporting-life-art-cards-collectibles/?fb_ref=528962056142023372%3Acba652a7869654e0e616">
- 			<img src="images/pintlogo.png" alt ="pinterest icon" id="pinLogo"/> 			
- 		</a>
-		</div>
 </footer>
+</body>
 </html>
