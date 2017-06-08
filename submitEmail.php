97621207c000444d3054ac5b6ba484753eb4fdeb
mnmays <mnmays@umich.edu>
2017-06-08 17:11:35 -0400

    email pop up

--- submitEmail.php
+++ submitEmail.php
@@ -3,59 +3,78 @@
 		
 	</head>
 	<body>
-		<?php
+<?php
 	
 	//declare variable
 	$email = $_GET["email"];
 	//echo $email; 
 	
-	//connection
+//connection
+	$dataSourceName = 'mysql:host=localhost;dbname=sportinglife';
+	$username = 'root';
+	$password = 'M3g4nM4ys17';
 	
- $dbhost = "localhost";
- $dbuser = "root";
- $dbpass = "M3g4nM4ys17";
- $db = "sportinglife";
- 
- $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
- 
+	try {
+		$db = new PDO($dataSourceName, $username, $password);	
+echo 'Connected successfully';
+echo "<br>";
+	}
+	catch (PDOException $e) {
+		$error_message = $e->getMessage();
+		echo 'Error. Cannot connect to database';
+echo "<br>";
+		exit();  
+	}
+
+
+echo 'Connected successfully';
+echo "<br>"; 
+
  $sql = "SELECT * FROM emailList";
- $result = $conn->query($sql);
- 
- if ($result->num_rows > 0)
- {
- 	while($row = $result->fetch_assoc())
-	{
-		if($email == $row["address"])
+echo 'after sql'; 
+echo "<br>";
+
+
+$result = $db->query($sql);
+$result->execute();
+$allresults =$result->fetchAll();
+echo $allresults; 
+
+echo 'after =result';
+echo "<br>";
+
+
+
+
+foreach($allresults as $row) 
+{
+  $id = $row['address'];
+  echo $id; 
+  echo "<br>"; 
+  
+        if($email == $row["address"])
 		{
-			?>
-				<center><h3>This address is already in the system!</h3></center>
-			<?php	
+			print "<center><h3>This address is already in the system!</h3></center>"; 	
+			break; 
 		}
 		else
 		{
-			$sql = "INSERT INTO emailList (address)
-				VALUES ('$email')";
-				
-			if ($conn->query($sql) === TRUE) 
-			{
-				?>
-				<center><h3>Success!</h3></center>
-				<p>
-					<center><img src="images/success.png-c200"></center>
-				</p>
-				<?php
-			} 
-			else 
-			{
-				?>
-				<center><h3>Unfortunatly, your email could not be added to the list.</h3></center>
-				<?php
-			} 
+			$sql = "INSERT INTO emailList (address) VALUES ('$email')";
+			$insertquery = $db-> prepare($sql);
+			$insertquery->execute();	
+
+			print "<center><h3>Success!</h3></center>"; 
+
 		}
+}
+
+echo "<br>";
+echo ' after echo result';
+echo "<br>";
 		
-		break; 
-	}
- }
+
+echo 'end of file ';
+echo "<br>"; 
  ?>
 	</body>
 </html>
